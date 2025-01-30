c398e62ada231af5a8b1fde4aae427f3b745e55e
dheeraj7866 <93239707+dheeraj7866@users.noreply.github.com>
2025-01-31 01:15:38 +0530

    addes

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -44,3 +44,27 @@ resource "aws_subnet" "private_subnet_2" {
     Name = "private-subnet-2"
   }
 }
+
+# Create an internet gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create route table for public subnet
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create a route to the internet gateway in the public route table
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id            = aws_internet_gateway.internet_gateway.id
+}
+
+# Associate public subnets with the public route table
+resource "aws_route_table_association" "public_route_association" {
+  count              = length(aws_subnet.public)
+  subnet_id         = aws_subnet.public[count.index].id
+  route_table_id    = aws_route_table.public_route_table.id
+}
