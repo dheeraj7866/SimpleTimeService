2f520c539f9024029ef3d7f4c81a608c29a286bc
dheeraj7866 <93239707+dheeraj7866@users.noreply.github.com>
2025-01-31 00:59:44 +0530

    s

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -53,8 +53,10 @@ resource "aws_iam_policy" "ecs_task_execution_policy" {
   })
 }
 
+
 resource "aws_iam_policy_attachment" "ecs_task_execution_role_attachment" {
-  
-  roles      = aws_iam_role.ecs_execution_role.name
+  name       = "ecs-task-execution-role-attachment"
   policy_arn = aws_iam_policy.ecs_task_execution_policy.arn
+  roles      = [aws_iam_role.ecs_execution_role.name]
 }
+
