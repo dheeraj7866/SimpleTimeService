0561a8d11f7b8d8465d1b5f9118eb5b40dabad39
dheeraj7866 <93239707+dheeraj7866@users.noreply.github.com>
2025-01-31 00:56:56 +0530

    added

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -55,5 +55,5 @@ resource "aws_iam_policy" "ecs_task_execution_policy" {
 
 resource "aws_iam_policy_attachment" "ecs_task_execution_role_attachment" {
   policy_arn = aws_iam_policy.ecs_task_execution_policy.arn
-  roles      = [aws_iam_role.ecs_execution_role.name]
+  roles      = aws_iam_role.ecs_execution_role.name
 }
